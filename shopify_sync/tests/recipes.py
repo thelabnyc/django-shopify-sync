9d66b4186afbc1bb2f877cffbd93efab6b4eba06
Craig Weber <crgwbr@gmail.com>
2024-08-07 17:30:34 -0400

    chore: migrate from model-mommy to model-bakery
    See https://model-bakery.readthedocs.io/en/latest/migrating_from_mommy.html

--- shopify_sync/tests/recipes.py
+++ shopify_sync/tests/recipes.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from model_mommy.recipe import Recipe, seq
+from model_bakery.recipe import Recipe, seq
 
 from ..models import Session, SmartCollection
 
